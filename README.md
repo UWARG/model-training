992f61f61489a7aa4323795b9c39c07c0d979f84
Xierumeng <xierumeng@hotmail.com>
2023-06-06 22:47:07 -0400

    Landing pad training code (#1)
    * Landing pad training code
    
    * Moved directory and updated instructions
    
    * Gitignore Pytorch file extension
    
    * Documentation update
    
    * Jetson Pytorch version

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# ML Training
+# ML Model Training
 
 Using Ultralytics YOLOv8.
+
+## Training
+
+Follow the [instructions](https://uwarg-docs.atlassian.net/l/cp/XBXNaCnx).
---
+++ training/training.py
@@ -0,0 +1,38 @@
+"""
+For model training
+"""
+
+import ultralytics
+
+
+# n for nano, s for small, m for medium, l for large
+# Use nano or small for now
+MODEL_START = "yolov8n.yaml"
+
+# Change path to last.pt in runs/ to resume
+MODEL_RESUME_PATH = "path/to/last.pt"
+
+# Path to configuration that describes the dataset directory structure
+DATASET_PATH = "2023_pad.yaml"
+
+
+if __name__ == "__main__":
+
+    # Load model
+    # Start or resume
+    model = ultralytics.YOLO(MODEL_START)
+
+    # Train
+    # Configurations: https://docs.ultralytics.com/usage/cfg/
+    model.train(
+        data=DATASET_PATH,
+        imgsz=720,
+        save=True,
+        save_period=10,
+        device=0,
+        workers=4,
+        verbose=True,
+        resume=True  # Ignored if model is not initialized with an existing .pt model
+    )
+
+    print("Done!")
