d6d30c7fad3dc9a52bc92b7a3fcd1c17686aaba9
Trotyl15 <mail@trotyl.xyz>
2023-10-23 14:14:24 -0400

    update username and remove ffmpeg

--- imaging/run.sh
+++ imaging/run.sh
@@ -6,16 +6,9 @@ v4l2-ctl -d /dev/video0 --set-ctrl white_balance_automatic=0
 v4l2-ctl -d /dev/video0 --set-ctrl white_balance_temperature=6500
 v4l2-ctl -d /dev/video0 --set-ctrl auto_exposure=1
 
-# Comment out one of the following
-
 # OpenCV Python
 touch /home/warg/record.log  # To confirm script has run
 cd /home/warg/model-training/
 source venv/bin/activate
 cd imaging/
 python -m imaging
-
-# FFMPEG video
-# touch /home/warg/record.log  # To confirm script has run
-# cd /home/warg/model-training/imaging/
-# ./video.sh
