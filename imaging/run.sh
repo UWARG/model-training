7b87b94c0bdc900709d899f89b49b9c775ed3413
mgupta27 <mihir_gupta@rocketmail.com>
2023-11-11 20:32:31 -0500

    Added extra v4l2 setting to run script

--- imaging/run.sh
+++ imaging/run.sh
@@ -4,6 +4,7 @@
 v4l2-ctl -d /dev/video0 --set-ctrl gain=0
 v4l2-ctl -d /dev/video0 --set-ctrl white_balance_temperature=4600
 v4l2-ctl -d /dev/video0 --set-ctrl white_balance_automatic=1
+v4l2-ctl -d /dev/video0 --set-ctrl auto_exposure=3
 
 # OpenCV Python
 touch /home/warg/record.log  # To confirm script has run
