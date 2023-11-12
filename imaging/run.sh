9679b0160f4ae14b06f4ef85936d58f79c984241
mgupta27 <79874172+mgupta27@users.noreply.github.com>
2023-11-11 20:36:06 -0500

    Set camera calibration to default settings (#5)
    * Set camera calibration to default settings
    
    * Added extra v4l2 setting to run script

--- imaging/run.sh
+++ imaging/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Colour calibration 2023-10-12
-v4l2-ctl -d /dev/video0 --set-ctrl hue=-128
-v4l2-ctl -d /dev/video0 --set-ctrl white_balance_automatic=0
-v4l2-ctl -d /dev/video0 --set-ctrl white_balance_temperature=6500
-v4l2-ctl -d /dev/video0 --set-ctrl auto_exposure=1
+# Camera calibration
+v4l2-ctl -d /dev/video0 --set-ctrl gain=0
+v4l2-ctl -d /dev/video0 --set-ctrl white_balance_temperature=4600
+v4l2-ctl -d /dev/video0 --set-ctrl white_balance_automatic=1
+v4l2-ctl -d /dev/video0 --set-ctrl auto_exposure=3
 
 # OpenCV Python
 touch /home/warg/record.log  # To confirm script has run
