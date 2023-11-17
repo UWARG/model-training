c4aed56f3b42646c7b56a78536ed433c8ecd3a36
Trotyl15 <mail@trotyl.xyz>
2023-11-17 00:11:13 -0500

    comment fix

--- imaging/run.sh
+++ imaging/run.sh
@@ -6,8 +6,8 @@ v4l2-ctl -d /dev/video0 --set-ctrl white_balance_automatic=1
 v4l2-ctl -d /dev/video0 --set-ctrl auto_exposure=3
 
 # OpenCV Python
-touch /home/warg/record.log  # To confirm script has run # TODO: Update
-cd /home/warg/model-training/ # TODO: Update
+touch /home/warg/record.log  # To confirm script has run  # TODO: Update
+cd /home/warg/model-training/  # TODO: Update
 source venv/bin/activate
-cd imaging/ # TODO: Update
-python -m imaging # TODO: Update
+cd imaging/  # TODO: Update
+python -m imaging  # TODO: Update
