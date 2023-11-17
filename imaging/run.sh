9a56e50725ad515168ff8527f8cfdfeb61e94181
Trotyl15 <mail@trotyl.xyz>
2023-11-17 00:41:59 -0500

    update comments

--- imaging/run.sh
+++ imaging/run.sh
@@ -6,8 +6,8 @@ v4l2-ctl -d /dev/video0 --set-ctrl white_balance_automatic=1
 v4l2-ctl -d /dev/video0 --set-ctrl auto_exposure=3
 
 # OpenCV Python
-touch /home/warg/record.log  # To confirm script has run  # TODO: Update to actual path
-cd /home/warg/model-training/  # TODO: Update to actual path
+touch /home/warg/record.log  # To confirm script has run  # Update to actual path
+cd /home/warg/model-training/  # Update to actual path
 source venv/bin/activate
 cd imaging/
 python -m imaging
