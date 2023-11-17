f1fd05d9a41b92eecb546ba5f592f19270c46644
Trotyl15 <mail@trotyl.xyz>
2023-11-16 23:37:35 -0500

    add todos and update run.sh

--- imaging/run.sh
+++ imaging/run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Camera calibration
-v4l2-ctl -d /dev/video0 --set-ctrl gain=0
 v4l2-ctl -d /dev/video0 --set-ctrl white_balance_temperature=4600
 v4l2-ctl -d /dev/video0 --set-ctrl white_balance_automatic=1
 v4l2-ctl -d /dev/video0 --set-ctrl auto_exposure=3
 
 # OpenCV Python
-touch /home/warg/record.log  # To confirm script has run
-cd /home/warg/model-training/
+touch /home/warg/record.log  # To confirm script has run # TODO: Update
+cd /home/warg/model-training/ # TODO: Update
 source venv/bin/activate
-cd imaging/
-python -m imaging
+cd imaging/ # TODO: Update
+python -m imaging # TODO: Update
