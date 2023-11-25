4abb84a20a7818072f7e0fd7e7446be2d11aa649
Xierumeng <xierumeng@hotmail.com>
2023-11-24 18:49:50 -0800

    Restore FFMPEG script (#9)
    * Restore FFMPEG script
    
    * Preference order

--- imaging/run.sh
+++ imaging/run.sh
@@ -5,9 +5,14 @@ v4l2-ctl -d /dev/video0 --set-ctrl white_balance_temperature=4600
 v4l2-ctl -d /dev/video0 --set-ctrl white_balance_automatic=1
 v4l2-ctl -d /dev/video0 --set-ctrl auto_exposure=3
 
+# Get current Unix time to avoid collision
+# TODO: Something more sophisticated (e.g. counter)
+TIME="${date +%s}"
+echo $TIME
+
 # OpenCV Python
-touch /home/warg/record.log  # To confirm script has run  # Update to actual path
 cd /home/warg/model-training/  # Update to actual path
+touch record_$TIME.log  # To confirm script has run
 source venv/bin/activate
 cd imaging/
 python -m imaging
