e01c64e90ab620e318df4d5cd854ff2b26c92b67
Xierumeng <xierumeng@hotmail.com>
2024-03-10 23:20:33 -0400

    Formatting tools

--- training.py
+++ training.py
@@ -15,24 +15,40 @@ MODEL_RESUME_PATH = "path/to/last.pt"
 # Path to configuration that describes the dataset directory structure
 DATASET_PATH = "2023_pad.yaml"
 
+# Model hyperparameters
+# Configurations: https://docs.ultralytics.com/usage/cfg/
+IMAGE_DIMENSION = 720
+SAVE_EVERY_NTH_EPOCH = 10
+DEVICE = 0  # CUDA device
+WORKER_COUNT = 4  # Worker threads
 
-if __name__ == "__main__":
 
+def main() -> int:
+    """
+    Main function
+    """
     # Load model
-    # Start or resume
+    # Start with MODEL_START or resume with MODEL_RESUME_PATH
     model = ultralytics.YOLO(MODEL_START)
 
     # Train
-    # Configurations: https://docs.ultralytics.com/usage/cfg/
     model.train(
         data=DATASET_PATH,
-        imgsz=720,
+        imgsz=IMAGE_DIMENSION,
         save=True,
-        save_period=10,
-        device=0,
-        workers=4,
+        save_period=SAVE_EVERY_NTH_EPOCH,
+        device=DEVICE,
+        workers=WORKER_COUNT,
         verbose=True,
         resume=True,  # Ignored if model is not initialized with an existing .pt model
     )
+
+    return 0
+
+
+if __name__ == "__main__":
+    result_main = main()
+    if result_main < 0:
+        print(f"ERROR: Status code: {result_main}")
 
     print("Done!")
